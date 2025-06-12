d406bcb700bc87380b46e00cd28e17c4f68b54ab
Nil Patel <112705642+nilp12200@users.noreply.github.com>
2025-06-12 15:49:16 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://localhost:3001/', {
+      const res = await axios.post('http://localhost:3001/api/login', {
         username: username.trim(),   // ✅ Trim to avoid space errors
         password: password.trim()
       });
