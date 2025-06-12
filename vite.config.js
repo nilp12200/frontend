d0dc6610715efe5cb364cae151b17d3adc3b392f
Nil Patel <112705642+nilp12200@users.noreply.github.com>
2025-06-12 15:34:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+VITE_API_URL=https://backend-0dxg.onrender.com
 
 // https://vite.dev/config/
 export default defineConfig({
